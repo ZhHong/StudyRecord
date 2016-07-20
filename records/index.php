e8a7e18071ed4b0d9e61e5e9fbcdeec36b93fbaa
Monkey <371816216@qq.com>
2016-07-20 18:15:15 +0800

    web

---
+++ records/index.php
@@ -0,0 +1,12 @@
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en",lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html;charset=utf8">
+</head>
+<body>
+	<p>This is standard HTML.....</p>
+	<?php
+	echo " this was generated using PHP!";
+	phpinfo();
+	?>
+</body>
+</html>
